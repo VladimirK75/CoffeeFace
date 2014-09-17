67e96a47530824604a2af5cf65c4178fd372d633
Vladimir <kruglov.vladimir@mail.ru>
2014-09-17 13:14:28 +0400

    Update dt_test.sh

--- dt_test.sh
+++ dt_test.sh
@@ -4,16 +4,19 @@ HOSTS="$1";
 [ -z "$HOSTS" ]  && echo "The first parameter must be full name of the file with list of hosts!" && exit;
 
 TXRATE="$2";
-[ -z "$TXRATE" ] && echo "The third parametet must be number of txRate option (like 500)" && exit;
+[ -z "$TXRATE" ] && echo "The 2-d parametet must be number of txRate option (like 500)" && exit;
 
-DOMAIN="$3";
-[ -z "$DOMAIN" ] && echo "The second parameter must be DOMAIN name (like \"uk.db.com\") !" && exit;
+TXDURATION="$3";
+[ -z "$TXRATE" ] && echo "The 3-d parametet must be number of txDurationMs option (like 600000 = 10min)" && exit;
+
+DOMAIN="$4";
+[ -z "$DOMAIN" ] && echo "The 4-th parameter must be DOMAIN name (like \"uk.db.com\") !" && exit;
 
 JAVA_HOME=/local/apps/cache/JRE_LINUX_7;
 JAVA=$JAVA_HOME/bin/java;
 RUN_DIR=/home/dfluser/datagram/;
 WGET_CMD="wget http://gmrepo.gslb.db.com:8481/nexus-webapp/content/groups/gtrepositories/com/oracle/coherence/coherence/3.7.1.12/coherence-3.7.1.12.jar -O coherence-3.7.1.12.jar";
-DT_RXTX_OPT="-txDurationMs 30000 -txRate $TXRATE -rxBufferSize 250 -packetSize 8192";
+DT_RXTX_OPT="-txDurationMs $TXDURATION -txRate $TXRATE -rxBufferSize 250 -packetSize 8192";
 DT_JAVA_OPT="-cp coherence-3.7.1.12.jar -server com.tangosol.net.DatagramTest";
 
 HN=0;
@@ -45,5 +48,5 @@ done;
 rm report.html
 for host in `cat $HOSTS`
 do
-grep -e "Clients have stopped." -e "Lifetime:" -e "Rx from publisher" -e "success rate" dt_$host.log | grep "Lifetime" -A 11 | tail -n 11 | awk '{getline a; printf "%-s\n", $0 " " a}' | sort | uniq | awk '{print $4,":",$7}' | tr -d ' '| tr -d '/' | tr -d 'success' | awk -F":" '{print "'"$host"'","\t",$1,"\t",$3}'| tr -d ' ' >> report.html;
+grep -e "Clients have stopped." -e "Lifetime:" -e "Rx from publisher" -e "success rate" dt_$host.log | grep "Lifetime" -A 11 | tail -n 11 | awk '{getline a; printf "%-s\n", $0 " " a}' | sort | uniq | awk '{print $4,":",$7}' | tr -d ' '| tr -d '/' | tr -d 'success' | awk -F":" '{print "'"$host"'","#",$1,"#",$3}'| tr -d ' ' >> report.html;
 done;
