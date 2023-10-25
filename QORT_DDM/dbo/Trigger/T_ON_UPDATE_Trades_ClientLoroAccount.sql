3e7ed7a9de8898568d772be037b7860c6bfe1bf6
Vladimir <kruglov.vladimir@mail.ru>
2023-10-26 00:35:45 +0300

    Update and rename QORT_DDM/dbo/T_ON_UPDATE_Trades_ClientLoroAccount.sql to QORT_DDM/dbo/Trigger/T_ON_UPDATE_Trades_ClientLoroAccount.sql

---
+++ QORT_DDM/dbo/Trigger/T_ON_UPDATE_Trades_ClientLoroAccount.sql
@@ -0,0 +1,24 @@
+﻿create   trigger dbo.T_ON_UPDATE_Trades_ClientLoroAccount on dbo.ClientLoroAccount after update as
+begin
+    set nocount on
+    if not exists( select 1
+                     from inserted )
+       and not exists( select 1
+                         from deleted )
+        return
+         else
+        begin
+            insert into dbo.ClientLoroAccount_log
+            select *
+                 , AuditDateTime_log = getdate()
+                 , AuditActions = 'D'
+                 , AuditUser = suser_name()
+              from deleted
+            union all
+            select *
+                 , AuditDateTime_log = getdate()
+                 , AuditActions = 'I'
+                 , AuditUser = suser_name()
+              from inserted
+    end
+end
