d2c921a058956c7f99a297fad497c78c165e8a98
Vladimir <kruglov.vladimir@mail.ru>
2023-10-26 00:34:39 +0300

    Update and rename QORT_DDM/dbo/T_ON_UPDATE_Trades_SettlementRules.sql to QORT_DDM/dbo/Trigger/T_ON_UPDATE_Trades_SettlementRules.sql

---
+++ QORT_DDM/dbo/Trigger/T_ON_UPDATE_Trades_SettlementRules.sql
@@ -0,0 +1,24 @@
+﻿create   trigger T_ON_UPDATE_Trades_SettlementRules on dbo.Trades_SettlementRules after insert, update, delete as
+begin
+    set nocount on
+    if not exists (select 1
+                     from inserted)
+       and not exists (select 1
+                         from deleted) 
+        return
+       else
+        begin
+            insert into dbo.Trades_SettlementRules_Log
+            select *
+                 , AuditDateTime_log = getdate()
+                 , AuditActions = 'D'
+                 , AuditUser = suser_name()
+              from deleted
+            union all
+            select *
+                 , AuditDateTime_log = getdate()
+                 , AuditActions = 'I'
+                 , AuditUser = suser_name()
+              from inserted
+        end
+end
